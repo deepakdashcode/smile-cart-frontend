0f0efa3f720d26e5b675e19d2103e963908edc70
Deepak Kumar Dash <deepakkumardash@BigBinarys-MacBook-Pro.local>
2025-06-25 10:18:02 +0530

    Implemented Axios interceptors to set http headers and to transform response

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -18,8 +18,8 @@ const Product = () => {
 
   const fetchProduct = async () => {
     try {
-      const response = await productsApi.show();
-      setProduct(response.data);
+      const product = await productsApi.show();
+      setProduct(product);
     } catch (error) {
       console.log("An error occurred:", error);
     } finally {
@@ -35,14 +35,7 @@ const Product = () => {
     );
   }
 
-  const {
-    name,
-    description,
-    mrp,
-    offer_price: offerPrice,
-    image_urls: imageUrls,
-    image_url: imageUrl,
-  } = product;
+  const { name, description, mrp, offerPrice, imageUrls, imageUrl } = product;
   const totalDiscounts = mrp - offerPrice;
   const discountPercentage = ((totalDiscounts / mrp) * 100).toFixed(1);
 
