5561ce7b73db892d310fc5634fc06a14e02fa262
Deepak Kumar Dash <deepakkumardash@BigBinarys-MacBook-Pro.local>
2025-06-26 19:23:24 +0530

    half way done 14.5 start from tooltip

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,36 +1,41 @@
 import { LeftArrow } from "neetoicons";
 import { Typography } from "neetoui";
+import { keys } from "ramda";
 import { AiOutlineShoppingCart } from "react-icons/ai";
 import { useHistory } from "react-router-dom";
 import useCartItemsStore from "stores/useCartItemsStore";
 
 const Header = ({ title, shouldShowBackButton = true, actionBlock }) => {
   //   const { cartItems } = useCartItemsStore();
-  const cartItemsCount = useCartItemsStore(store => store.cartItems.length);
+  const cartItemsCount = useCartItemsStore(
+    store => keys(store.cartItems).length
+  );
   const history = useHistory();
 
   return (
-    <div className="m-2">
-      <div className="flex items-center">
-        {shouldShowBackButton && (
-          <LeftArrow
-            className="hover:neeto-ui-bg-gray-400 neeto-ui-rounded-full mr-6"
-            onClick={history.goBack}
-          />
-        )}
-        <Typography style="h1" weight="semibold">
-          {title}
-        </Typography>
-      </div>
-      <div className="flex items-end space-x-4">
-        {actionBlock}
-        <div className="flex flex-col">
-          {cartItemsCount > 0 && (
-            <span className="neeto-ui-border-black neeto-ui-rounded-full min-w-fit flex h-5 w-5 items-center self-end border p-1">
-              {cartItemsCount}
-            </span>
+    <div className="mb-2 mt-2">
+      <div className="flex flex-row items-center justify-between p-2">
+        <div className="flex items-center">
+          {shouldShowBackButton && (
+            <LeftArrow
+              className="hover:neeto-ui-bg-gray-400 neeto-ui-rounded-full mr-6"
+              onClick={history.goBack}
+            />
           )}
-          <AiOutlineShoppingCart size="2rem" />
+          <Typography style="h1" weight="bold">
+            {title}
+          </Typography>
+        </div>
+        <div className="flex items-end space-x-4">
+          {actionBlock}
+          <div className="flex flex-col">
+            {cartItemsCount > 0 && (
+              <span className="neeto-ui-border-black neeto-ui-rounded-full min-w-fit flex h-5 w-5 items-center self-end border p-1">
+                {cartItemsCount}
+              </span>
+            )}
+            <AiOutlineShoppingCart size="2rem" />
+          </div>
         </div>
       </div>
       <hr className="neeto-ui-bg-black h-1" />
---
+++ src/components/common/ProductQuantity.jsx
@@ -0,0 +1,40 @@
+import useSelectedQuantity from "hooks/useSelectedQuantity";
+import { Button } from "neetoui";
+
+const ProductQuantity = ({ slug, availableQuantity }) => {
+  const { selectedQuantity, setSelectedQuantity } = useSelectedQuantity(slug);
+
+  const isNotValidQuantity = selectedQuantity >= availableQuantity;
+
+  const preventNavigation = e => {
+    e.stopPropagation();
+    e.preventDefault();
+  };
+
+  return (
+    <div className="neeto-ui-border-black neeto-ui-rounded inline-flex flex-row items-center border">
+      <Button
+        className="focus-within:ring-0"
+        disabled={isNotValidQuantity}
+        label="-"
+        style="text"
+        onClick={e => {
+          preventNavigation(e);
+          setSelectedQuantity(selectedQuantity - 1);
+        }}
+      />
+      {selectedQuantity}
+      <Button
+        className="focus-within:ring-0"
+        label="+"
+        style="text"
+        onClick={e => {
+          preventNavigation(e);
+          setSelectedQuantity(selectedQuantity + 1);
+        }}
+      />
+    </div>
+  );
+};
+
+export default ProductQuantity;
